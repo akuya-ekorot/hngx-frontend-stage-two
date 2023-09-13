69fefc884c0e2cee01e7dc2b7f32b7aa428f0b58
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-13 14:18:57 +0300

    remove unused story

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -3,7 +3,7 @@ import MovieCard from "../MovieCard";
 
 const MovieList: React.FC<{ movies: Movie[] }> = (props) => {
   return (
-    <section className="flex flex-col gap-11">
+    <section className="flex flex-col gap-11 items-center">
       <header className="flex justify-between items-center">
         <h1 className="font-bold text-4xl">Featured Movies</h1>
         <button className="flex items-center gap-2 text-rose-700 text-lg">
@@ -11,7 +11,7 @@ const MovieList: React.FC<{ movies: Movie[] }> = (props) => {
           <ChevronRight />
         </button>
       </header>
-      <div className="grid grid-cols-4 gap-20">
+      <div className="grid grid-cols-4 w-full max-w-5xl">
         {props.movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}

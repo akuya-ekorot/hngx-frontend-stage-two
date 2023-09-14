100ac3d62e17d20a3f30f8350a2ea59197aef4c2
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-14 11:07:52 +0300

    rewrite type of movie

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,21 @@
 interface Genre {
-	id: number;
-	name: string;
+  id: number;
+  name: string;
 }
 
 interface Movie {
-	id: number;
-	name: string;
-	posterUrl: string;
-	releaseDate: string;
-	genres: Genre[];
-	countries: string[];
-	imdbId: string;
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }

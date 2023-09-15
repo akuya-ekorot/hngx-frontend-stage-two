1d8c95f2bda6a84f6c1c07b26f0f43cea1bcc065
Akuya Ekorot <akuyaekorot@gmail.com>
2023-09-15 08:58:33 +0300

    improve hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import api from "@/utils/api";
 import MovieList from "@/components/MovieList";
 import Hero from "@/components/Hero";
+import HeroDetails from "@/components/Hero/Details";
 
 const Home = async () => {
   const movies = await api.getPopularMovies();
-  const backdrops = movies.map((movie) => movie.backdrop_path);
+  const heroDetails = movies.slice(0, 5);
 
   return (
     <main className="w-full">
-      <Hero backdrops={backdrops} />
+      <Hero details={heroDetails} />
       <MovieList movies={movies} />
     </main>
   );
